a779adbbade6f9943ac02ac5dc8e2cfb2c26c655
Kassaundra Fequet <kassaundra.fequet@keyin.com>
2026-02-02 15:34:06 -0330

    completed exercise 1 & 2

---
+++ src/LinkedList/Main.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        SingleLinkedList singleLinkedList = new SingleLinkedList();
+        singleLinkedList.insertLinkedList(1,0);
+        singleLinkedList.insertLinkedList(2,1);
+        singleLinkedList.insertLinkedList(3,2);
+        singleLinkedList.insertLinkedList(4,3);
+        singleLinkedList.traverseLinkedList();
+
+        //Returns false
+        System.out.println(singleLinkedList.searchValue(5));
+        //Returns true
+        System.out.println(singleLinkedList.searchValue(3));
+
+        singleLinkedList.deleteLinkedList(2);
+        singleLinkedList.traverseLinkedList();
+
+
+
+
+
+    }
+}
---
+++ src/LinkedList/SingleLinkedList.java
@@ -0,0 +1,117 @@
+package LinkedList;
+
+public class SingleLinkedList {
+    public Node head;
+    public Node tail;
+    public int size;
+
+    public Node createSingleLinkedList(int nodeValue) {
+        Node node = new Node();
+        node.next = null;
+        node.value = nodeValue;
+        head = node;
+        tail = node;
+        size = 1;
+        return head;
+    }
+
+    //insert Method
+    public void insertLinkedList(int nodeValue, int location) {
+        Node node = new Node();
+        node.value = nodeValue;
+
+        if (head == null) {
+            createSingleLinkedList(nodeValue);
+            return;
+        } else if (location == 0) {
+            node.next = head;
+            head = node;
+        } else if (location >= size) {
+            node.next = null;
+            tail.next = node;
+            tail = node;
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while (index < location - 1) {
+                tempNode = tempNode.next;
+                index++;
+            }
+            Node nextNode = tempNode.next;
+            tempNode.next = node;
+            node.next = nextNode;
+        }
+        size++;
+    }
+
+    // Traversing through a linked list
+    public void traverseLinkedList() {
+        if (head == null) {
+            System.out.println("Linked list does not exist");
+        } else {
+            Node tempNode = head;
+            for (int i = 0; i < size; i++) {
+                System.out.print(tempNode.value);
+                if (i != size - 1) {
+                    System.out.print(" -> ");
+                }
+                tempNode = tempNode.next;
+
+            }
+            System.out.println("\n");
+
+        }
+    }
+
+    //EXERCISE 1: given a value, search for that element and return a boolean if the element exist or not.
+
+    public boolean searchValue(int nodeValue) {
+        Node tempNode = head;
+        while (tempNode != null) {
+            if (tempNode.value == nodeValue) {
+                return true;
+            }
+            tempNode = tempNode.next;
+        }
+        return false;
+    }
+
+    //EXERCISE 2: Given a location Implement a Delete method.
+
+    public void deleteLinkedList(int location) {
+
+        if (head == null) {
+            System.out.println("Linked list does not exist");
+            return;
+        } else if (location == 0) {
+            if (size == 1) {
+                head = null;
+                tail = null;
+                size = 0;
+                return;
+            }
+            head = head.next;
+            size--;
+        } else if (location >= size - 1) {
+            Node tempNode = head;
+            for (int i = 0; i < size - 2; i++) {
+                tempNode = tempNode.next;
+            }
+            if (tempNode == head && size == 1) {
+                head = tail = null;
+                size = 0;
+                return;
+            }
+            tempNode.next = null;
+            tail = tempNode;
+            size--;
+        } else {
+            Node tempNode = head;
+            for (int i = 0; i < location - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            tempNode.next = tempNode.next.next;
+            size--;
+        }
+    }
+}
